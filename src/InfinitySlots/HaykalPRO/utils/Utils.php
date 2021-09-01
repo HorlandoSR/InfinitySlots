101c144e6e12c0c653be4bf2d5db381b3b29706d
Haykal <76930536+HorlandoSR@users.noreply.github.com>
2021-09-01 09:43:00 +0700

    Add files via upload

---
+++ src/InfinitySlots/HaykalPRO/utils/Utils.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace InfinitySlots\HaykalPRO\utils;
+
+use JackMD\ConfigUpdater\ConfigUpdater;
+use JackMD\UpdateNotifier\UpdateNotifier;
+
+use pocketmine\utils\Config;
+
+use CustomQuery\iZeaoGamer\Main;
+
+use RuntimeException;
+
+class Utils { //Register class.
+
+    public function __construct(Main $plugin) { //A construction function, registering the main class to this class.
+        $this->plugin = $plugin; //Register variable
+    }
+     /** @var int */
+     private const CONFIG_VERSION = 0.2; //Register config version file - Will get updated everytime a new config version is available.
+	/**
+	 * Checks if a new version of this plugin is released.
+	 */
+    public function PluginUpdater(): void{
+        UpdateNotifier::checkUpdate($this->plugin, $this->plugin->getDescription()->getName(), $this->plugin->getDescription()->getVersion());
+    }
+	
+       /**
+	* Checks if a new config update is available for this plugin.
+	*/
+    public function ConfigUpdater(): void{
+        $config = new Config($this->plugin->getDataFolder() . "config.yml", Config::YAML, array());
+		ConfigUpdater::checkUpdate($this->plugin, $config, "config-version", self::CONFIG_VERSION);
+    }
+}
