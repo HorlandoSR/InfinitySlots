5ec916d5d5fa5e657cfc03f6e758f2bbae4012cf
Haykal <76930536+HorlandoSR@users.noreply.github.com>
2021-09-01 09:38:08 +0700

    Add files via upload

---
+++ src/InfinitySlots/HaykalPRO/Main.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace InfinitySlots\HaykalPRO;
+
+use pocketmine\event\server\QueryRegenerateEvent;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use pocketmine\plugin\PluginBase;
+use InfinitySlots\HaykalPRO\utils\Utils;
+
+class Main extends PluginBase implements Listener{
+
+	public function onEnable(){
+		$this->getLogger()->info("§aPlugin InfinitySlots Enable");
+        $this->getServer()->getPluginManager()->registerEvents($this,$this);
+
+        @mkdir($this->getDataFolder());
+       $this->saveDefaultConfig();
+       $this->getResource("config.yml");
+	}
+
+	public function onQuery(QueryRegenerateEvent $event){
+	    $config = new Config($this->getDataFolder() . "config.yml", Config::YAML, array());
+	if($config->get("infinity-slots") === true){
+        $event->setMaxPlayerCount($event->getPlayerCount() + 1);
+    }
+}
